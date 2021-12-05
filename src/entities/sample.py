ce6f0d915b38922ab7de2ca76989703ee62cc1e7
Sari Bäckman <sari.m.backman@gmail.com>
2021-12-05 22:42:27 +0200

    sekvenssikaavio

--- src/entities/sample.py
+++ src/entities/sample.py
@@ -14,10 +14,10 @@ class Sample:
         self.a1_cell = a1_cell
         self.b_cell = b_cell
         self.timestamp = timestamp
+        self.reaction_checker = ReactionLogic()
+        self.abo_checker = ABOLogic()
 
     def run_checks(self):
-        reaction_checker = ReactionLogic()
-        if reaction_checker.run_reaction_check(self.anti_a, self.anti_b, self.anti_d, self.control, self.a1_cell, self.b_cell) is True:
-            abo_checker = ABOLogic()
-            return abo_checker.run_abo_check(self.anti_a, self.anti_b, self.anti_d, self.a1_cell, self.b_cell)
-        return reaction_checker.run_reaction_check(self.anti_a, self.anti_b, self.anti_d, self.control, self.a1_cell, self.b_cell)
+        if self.reaction_checker.run_reaction_check(self.anti_a, self.anti_b, self.anti_d, self.control, self.a1_cell, self.b_cell) is True:
+            return self.abo_checker.run_abo_check(self.anti_a, self.anti_b, self.anti_d, self.a1_cell, self.b_cell)
+        return self.reaction_checker.run_reaction_check(self.anti_a, self.anti_b, self.anti_d, self.control, self.a1_cell, self.b_cell)
